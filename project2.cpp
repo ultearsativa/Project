5d4e0bc04594e22bc663c62e162935f1b0429a16
RafiRashya <121283174+RafiRashya@users.noreply.github.com>
2023-01-13 15:23:02 +0700

    Update project2.cpp
    Rules dan persetujuan start permainan

--- project2.cpp
+++ project2.cpp
@@ -9,16 +9,30 @@ int main() {
 
   // Buat angka acak yang akan ditebak
   int angka_acak = rand() % 100 + 1;
-  string p1,p2;
-  // Variabel untuk menyimpan tebakan Player
+  //Variabel input nama Player dan persetujuan permainan
+  string p1,p2, bermain;
   // Variabel untuk menyimpan tebakan Player
   int tebakan,tebakan2;
   bool benar = false;
+  
+  cout << "SELAMAT DATANG DI PERMAINAN TEBAK ANGKA!!!" << endl;
+  cout << "====================PERATURAN PERMAINAN====================" << endl;
+  cout << "1. Anda harus menebak angka dengan lingkup 1-100" << endl;
+  cout << "2. Permainan ini membutuhkan 2 pemain" << endl;
+  cout << "3. Jika tebakan pemain terlalu tinggi/rendah akan muncul keterangan di bawahnya" << endl;
+  cout << "4. Jika tebakan pemain benar akan ada keterangan dan permainan akan berakhir" << endl;
+  cout << "===========================================================" << endl;
+  cout << "Apakah Kalian ingin bermain ? (Ya atau Tidak) : ";
+  cin >> bermain;
+  
+  if (bermain == "ya"){
+  cout << "\n=================SELAMAT BERMAIN===================" << endl;
   cout << "Masukkan Nama Player 1 : ";
   cin >> p1;
   cout << "Masukkan Nama Player 2 : ";
   cin >> p2;
   cout << "---------------------------------" << endl;
+  
   // Loop sampai salah satu Player menebak angka yang benar
   while (!benar) {
     cout << p1 << ", silakan tebak angka (1-100): ";
@@ -46,6 +60,12 @@ int main() {
       cout << "Tebakan " << p2 << " terlalu tinggi." << endl << "---------------------------------" << endl;
     }
     }
+        
+  } 
+  
+  else if (bermain == "tidak"){
+  	cout << "SAMPAI BERTEMU DILAIN KESEMPATAN!";
+  }
 
   return 0;
 }
